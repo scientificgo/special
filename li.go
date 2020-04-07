e36fca4f5952547f02f57c269917b3f40ec0f797
Jack Parkinson <jdparkinson93@gmail.com>
2020-04-07 16:11:28 +0100

    improve Li accuracy for large x, fix test outputs

--- li.go
+++ li.go
@@ -14,4 +14,28 @@ import "math"
 //  Li(+Inf) = +Inf
 //  Li(x < 0) = NaN
 //
-func Li(x float64) float64 { return Ei(math.Log(x)) }
+func Li(x float64) float64 {
+	switch {
+	case math.IsInf(x, 1):
+		return x
+	case x < 0 || math.IsNaN(x):
+		return nan
+	}
+
+	l := math.Log(x)
+
+	if l <= 40 {
+		return Ei(math.Log(x))
+	}
+
+	// For large x, use the asymptotic series for Ei
+	// with the substitution x -> ln(x) to avoid additional
+	// rounding errors.
+
+	//           x    ∞     k!
+	// Li(x) ~ -----  Σ  --------
+	//         ln(x) k=0 ln(x)**k
+
+	y := 1 / l
+	return x * y * factorialseries(y)
+}
