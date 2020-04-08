fb9ce51854beb694d3e6cab9542979b56f94c2e4
Jack Parkinson <jdparkinson93@gmail.com>
2020-04-08 22:33:37 +0100

    add accurate Beta function

---
+++ beta.go
@@ -0,0 +1,110 @@
+// Copyright (c) 2020, Jack Parkinson. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause
+// license that can be found in the LICENSE file.
+
+package special
+
+import "math"
+
+// Beta returns the Beta function of x and y.
+//
+// Special cases are:
+//  Beta(±Inf, y > 0) = 0
+//  Beta(±Inf, y ≤ 0) = NaN for integer y
+//  Beta(-Inf, y < 0) = NaN for non-integer y
+//  Beta(+Inf, y < 0) = ±Inf for non-integer y
+//  Beta(x ≤ 0, y ≤ 0) = NaN for integer x and y
+//  Beta(x ≤ 0, y) = NaN for integer x and non-integer y or integer y > -x
+//  Beta(x, y) = 0 for non-integer x and y and integer x+y ≤ 0
+func Beta(x, y float64) float64 {
+	// special cases
+	if math.IsInf(x, 0) || math.IsInf(y, 0) {
+		if math.IsInf(y, 0) {
+			x, y = y, x
+		}
+		switch {
+		case y > 0:
+			return 0
+		case x < 0:
+			return nan
+		case isNonPosInt(y):
+			return nan
+		case math.Mod(math.Trunc(y-1), 2) == 0: // if even when rounded away from 0
+			return +inf
+		default:
+			return -inf
+		}
+	}
+
+	z := x + y
+
+	ix := isNonPosInt(x)
+	iy := isNonPosInt(y)
+	iz := isNonPosInt(z)
+
+	if iy && !ix { // wlog, swap x and y
+		x, y = y, x
+		ix, iy = iy, ix
+	}
+
+	sign := 1
+	// non-positive integer cases
+	if ix && (iy || !iz) { // all terms diverge or only Gamma(x) diverges
+		return nan
+	}
+	if iz {
+		if !ix { // only Gamma(x+y) diverges
+			return 0
+		}
+
+		// Both Gamma(x) and Gamma(x+y) diverge, but their ratio
+		// has a finite limit:
+		//
+		//   Gamma(x)    Gamma(1-x-y)       y
+		//  ---------- = ------------ * (-1)
+		//  Gamma(x+y)    Gamma(1-x)
+		//
+
+		x, z = 1-z, 1-x
+		if math.Mod(y, 2) == 1 {
+			sign = -sign
+		}
+	}
+
+	// Beta(x, y) = Gamma(x) * Gamma(y) / Gamma(x+y)
+
+	if y < x { // let y ≥ x wlog
+		x, y = y, x
+	}
+
+	// avoid overflows and improve accuracy by reducing arguments
+	if math.Abs(x+y) > 50 || math.Abs(x) > 50 || math.Abs(y) > 50 {
+
+		// use the Gauss multiplication formula with x = 2*x', etc., giving
+		//
+		//               Beta(x', y') * Beta(x'+1/2, y'+1/2)    Gamma(z'+1)
+		//  Beta(x, y) = ----------------------------------- * -------------
+		//                           2 * Sqrt(π)               Gamma(z'+1/2)
+		//
+
+		x /= 2
+		y /= 2
+		z /= 2
+
+		// if z is still very large, use the asymptotic series for the remaining Gamma terms
+
+		if z > 170 {
+			s := math.Sqrt(z)
+			s += poleval(1/z, 1./8, 1./128, -5./1024, -21./32768, 399./262144) / s
+			return 0.5 * Beta(x, y) * Beta(x+0.5, y+0.5) * s / math.SqrtPi * float64(sign)
+		}
+
+		return 0.5 * Beta(x, y) * Beta(x+0.5, y+0.5) * math.Gamma(z+1) / (math.Gamma(z+0.5) * math.SqrtPi) * float64(sign)
+	}
+
+	// direct formula
+	gx := math.Gamma(x)
+	gy := math.Gamma(y)
+	gz := math.Gamma(z)
+	return gx * (gy / gz) * float64(sign)
+}
---
+++ beta_test.go
@@ -0,0 +1,60 @@
+// Copyright (c) 2020, Jack Parkinson. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause
+// license that can be found in the LICENSE file.
+
+package special_test
+
+import (
+	"testing"
+
+	. "scientificgo.org/special"
+	"scientificgo.org/testutil"
+)
+
+const tolBeta = 4.5 * Macheps
+
+var casesBeta = []struct {
+	Label         string
+	In1, In2, Out float64
+}{
+	{"", 1, 10, 0.1},
+	{"", 19.125, 11.0125, 2.4472541189858992560452364E-9},
+	{"", 80, 100, 7.480703996850429143482604E-55},
+	{"", 171, 171, 3.0280694105736877516074272E-104}, // asymptotic expansion
+
+	{"", 8.25, -40.75, 1.3524462378721093720166038E-9},
+	{"", 100., -98.5, -35.315948806233095764437743},
+	{"", 100, -101.5, 1.3048090539541725894540623E-5},
+
+	// special cases
+	{"sc", 1, -2, -0.5},  // Beta(1, -2) = -1/2
+	{"sc", 10, -10, 0.1}, // Beta(10, -10) = 1/10
+	{"sc", -3.5, 2.5, 0}, // Beta(x, y) = 0 for non-positive integer x+y and not x or y
+	{"sc", -1, -4, NaN},  // Beta(-1, -4) = +Inf
+
+	{"sc", +Inf, +Inf, 0},   // Beta(+Inf, +Inf) = 0
+	{"sc", +Inf, -Inf, 0},   // Beta(+Inf, -Inf) = 0
+	{"sc", -Inf, -Inf, NaN}, // Beta(-Inf, -Inf) = NaN
+
+	{"sc", +Inf, 1.1, 0},     // Beta(+Inf, x > 0) = 0
+	{"sc", +Inf, -1.1, +Inf}, // Beta(+Inf, x < 0) = ±Inf for non-integer x
+	{"sc", +Inf, -0.1, -Inf}, // Beta(+Inf, x < 0) = ±Inf for non-integer x
+	{"sc", +Inf, -2, NaN},    // Beta(+Inf, x ≤ 0) = +Inf for integer x
+
+	{"sc", -Inf, 1.1, 0},    // Beta(-Inf, x > 0) = 0
+	{"sc", -Inf, -0.1, NaN}, // Beta(-Inf, x ≤ 0) = NaN
+
+	{"", NaN, 1, NaN},
+}
+
+func TestBeta(t *testing.T) { testutil.Test(t, tolBeta, casesBeta, Beta) }
+
+func BenchmarkBeta(b *testing.B) {
+	for _, c := range casesBeta {
+		b.Run(c.Label, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = Beta(c.In1, c.In2)
+			}
+		})
+	}
+}
