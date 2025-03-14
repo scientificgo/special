ca7feb6168348738b944db0698825db35438139e
ijxy <32483798+ijxy@users.noreply.github.com>
2025-03-14 01:10:46 +0000

    chore: remove additional license header from each file

--- utils.go
+++ utils.go
@@ -1,7 +1,3 @@
-// Copyright (c) 2018, Jack Parkinson. All rights reserved.
-// Use of this source code is governed by the BSD 3-Clause
-// license that can be found in the LICENSE file.
-
 package special
 
 // removeCommonElements filters the slices a and b to remove any elements in both.
